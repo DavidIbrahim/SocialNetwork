ed9e39b5a897509302d1e777911820516cb583da
MinaRizk <dvsvsvfssgds@gmail.com>
2018-04-17 15:16:20 +0200

    LoadandSave solved issues

--- src/GraphClasses/Main.java
+++ src/GraphClasses/Main.java
@@ -1,18 +1,42 @@
 package GraphClasses;
 
+import David.ProjectExceptions;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.InputStreamReader;
+
+import static GraphClasses.SaveAndLoad.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Mina");
-        // write your code here
+    public static void main(String[] args) throws ProjectExceptions.AccountException {
 
+	    System.out.println("Mina");
 
         SocialGraph graph = new SocialGraph();
 
+        try {
+            graph.addNewAccount("mina","mina");
+        } catch (ProjectExceptions.AccountException e) {
+            e.printStackTrace();
+        }
+
+        graph.addNewAccount("david","david");
+
+
+            SaveAndLoad SL = new SaveAndLoad();
+                SL.Save(graph);
 
 
 
-    }
+
+        }
 
     public int x(){return 5;}
+
+
+
+
+
 }
--- src/GraphClasses/SaveAndLoad.java
+++ src/GraphClasses/SaveAndLoad.java
@@ -1,15 +1,40 @@
 package GraphClasses;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+/* import java.io.InputStreamReader; */
+import java.io.Reader;
+
+
 public class SaveAndLoad {
 
-    public static  void Load (){
+    public static  void Save  ( SocialGraph x     )
+           {
+               Writer writer = null;
+               try {
+                   writer = new FileWriter("SocialGraph.json");
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+               System.out.println("saving");
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(x, writer);
+            System.out.println("saved");
 
 
     }
 
-    public static  void Save (){
 
 
+    public static  void Load (  SocialGraph x , Reader reader    )
+    {
+            System.out.println("loading");
+            Gson gson = new GsonBuilder().create();
+            x = gson.fromJson(reader, SocialGraph.class);
+            System.out.println("loaded");
     }
 
 }
